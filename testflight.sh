9982e94ddee32c162d1db540b1bc9ce345c4e680
Akshay Sud <sudakshay@gmail.com>
2018-04-11 09:56:41 +1200

    Delete testflight.sh

--- testflight.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-  echo "This is a pull request. No deployment will be done."
-  exit 0
-fi
-if [[ "$TRAVIS_BRANCH" != "build_testflight" ]]; then
-  echo "Testing on a branch other than build_testflight. No deployment will be done."
-  exit 0
-fi
