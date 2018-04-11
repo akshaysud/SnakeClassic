6214f9b1231085498597b6a068cc8d4655afd63a
Akshay Sud <sudakshay@gmail.com>
2018-04-11 12:26:36 +1200

    Duh - how would have it worked. It will still fail for now

--- scripts/travis/testflight.sh
+++ scripts/travis/testflight.sh
@@ -13,3 +13,24 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_NAME.mobileprovision"
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 OUTPUTDIR="/Users/travis/build"
+
+echo "********************"
+echo "*     Signing      *"
+echo "********************"
+xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+RELEASE_NOTES="This version was uploaded automagically by Travis\nTravis Build number: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+zip -r -9 "$OUTPUTDIR/$APPNAME.app.dSYM.zip" "$OUTPUTDIR/$APPNAME.app.dSYM"
+
+echo "********************"
+echo "*    Uploading     *"
+echo "********************"
+curl http://testflightapp.com/api/builds.json \
+  -F file="@$OUTPUTDIR/$APPNAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APPNAME.app.dSYM.zip" \
+  -F api_token="$API_TOKEN" \
+  -F team_token="$TEAM_TOKEN" \
+  -F distribution_lists=$DISTRIBUTION_LISTS \
+  -F notes="$RELEASE_NOTES" -v \
+  -F notify="FALSE"
