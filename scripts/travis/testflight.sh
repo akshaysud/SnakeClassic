d296c3b721d922da29b2d7d7e2bffcaf9dc8c21d
Akshay Sud <sudakshay@gmail.com>
2018-04-11 12:04:04 +1200

    Update testflight.sh

--- scripts/travis/testflight.sh
+++ scripts/travis/testflight.sh
@@ -3,7 +3,7 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
   exit 0
 fi
-if [[ "$TRAVIS_BRANCH" != "build_testflight" ]]; then
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
   echo "Testing on a branch other than build_testflight. No deployment will be done."
   exit 0
 fi
