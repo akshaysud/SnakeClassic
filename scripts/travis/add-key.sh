518417f3db6249694880e1cfa5d59f2d7cec54ec
Akshay Sud <sudakshay@gmail.com>
2018-04-30 14:34:51 +1200

    Update add-key.sh

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -5,6 +5,6 @@ security unlock-keychain -p travis ios-build.keychain
 security -v set-keychain-settings -lut 86400 ios-build.keychain
 security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/travis/Certificates.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/travis/Certificates.p12 -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+#security import ./scripts/travis/Certificates.p12 -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
